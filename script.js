b2b35fdc5ad5788f91604d02abaf7fe7b8ce7ec0
Jane Anezi <110386328+janeAnezi@users.noreply.github.com>
2023-12-28 13:15:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ document.addEventListener('DOMContentLoaded', function() {
     const userNameInput = document.getElementById('userName');
     const greetingDiv = document.getElementById('greeting');
     const clearButton = document.getElementById('clearButton');
-  
+
+     const storedName = localStorage.getItem('user');
+    
+    //  if (storedName) {
+    //   displayGreeting(storedName);
+    // } else {
+    //   displayGreeting();
+    // }
+    
     nameForm.addEventListener('submit', function(event) {
       event.preventDefault();
       const userName = userNameInput.value;
       localStorage.setItem('user', userName);
-      displayGreeting();
+      displayGreeting(storedName);
     });
   
     clearButton.addEventListener('click', function() {
@@ -16,15 +24,12 @@ document.addEventListener('DOMContentLoaded', function() {
       displayGreeting();
     });
   
-    function displayGreeting() {
-      const storedName = localStorage.getItem('user');
-      if (storedName) {
-        greetingDiv.textContent = `Welcome back, ${storedName}!`;
+    function displayGreeting(name) {
+      if (name) {
+        greetingDiv.textContent = `Welcome back, ${name}!`;
       } else {
         greetingDiv.textContent = 'Hello there!';
       }
     }
-  
-    displayGreeting();
   });
   
